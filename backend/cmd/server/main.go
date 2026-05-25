a0146b99005724c94233d93936140ee2dbd07c30
ShioPy0101 <maruyama.function@gmail.com>
2026-05-25 18:57:01 +0900

    feat : main改良

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"net/http"
 
+	// "github.com/ShioPy0101/sql-playground/internal/pandasHandler"
+
+	"github.com/ShioPy0101/sql-playground/internal/handler/sqliteHandler"
+	"github.com/ShioPy0101/sql-playground/internal/service"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 
+	api := e.Group("/api")
+
+	sqliteAPI := api.Group("/sqlite")
+	// pandasAPI := api.Group("/pandas")
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Echo!")
 	})
+
+	sqliteService := service.NewSQLiteService()
+
+	sqliteHandlerInstance := sqliteHandler.NewSQLiteHandler(sqliteService)
+
+	sqliteAPI.POST("/execute", sqliteHandlerInstance.Execute)
+	// pandasAPI.GET("/examples", pandasHandler.ListExamples)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
